0e56c6ae21f8729a234e1488448b79a8281f2ecd
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-21 20:16:39 +0530

    commit

--- 01_practice_set_Q2.c
+++ 01_practice_set_Q2.c
@@ -3,7 +3,7 @@
 int main(){
     int radius;
     scanf("%d", &radius);
-    printf("Enter the value of radius");
+    printf("Enter the value of radius: %d");
     printf("The area of the circle is: %f", 3.14 * radius * radius);
     
     return 0;
