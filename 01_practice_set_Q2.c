461781b85025c34f1ce90eb24830bdafdd5d105e
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-21 20:25:11 +0530

    commit

--- 01_practice_set_Q2.c
+++ 01_practice_set_Q2.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int radius;
-    scanf("%d", &radius);
-    printf("Enter the value of radius: %d");
-    printf("The area of the circle is: %f", 3.14 * radius * radius);
-    
+    int radius = 5;
+    int height = 10;
+    printf("The area of the circle is: %f\n", 3.14 * radius * radius);
+    printf("The volume of the cylinder:%f\n",3.14 * radius* radius* height);
     return 0;
 }
