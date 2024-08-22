53687f9c4034f69fe60bcb5ac9317798e96601be
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-22 12:14:06 +0530

    commit

--- 01_practice_set_Q3.c
+++ 01_practice_set_Q3.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int main(){
-    int c=32.0,f;
-    f = ((9/5)*c)+32;
-    printf("The Temperature in Fahrenheit :%f",f);
+int main() {
+    float c, f;
+    printf("Enter the value in Celsius: ");
+    scanf("%f", &c);
+    
+    f = (9.0/5.0) * c + 32;
+    printf("The Temperature in Fahrenheit: %f", f);
+    
     return 0;
-
 }
