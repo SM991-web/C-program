7b2be9be5c1e41b8ef9a5cfc76fa697e3f36f56d
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-23 19:17:08 +0530

    commit

---
+++ Chapter 2/02_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 4;
+    int b = 3;
+    int c = 12;
+    int d = a+b+c;
+    printf("The value of a is %d b is %d c is %d and total is %d",a,b,c,d);
+    printf();
+    return 0;
+
+}
