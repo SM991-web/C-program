9f137b4d88d2bc047aface36af711e4a784d94cf
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-24 20:58:46 +0530

    commit

---
+++ Chapter 2/03_main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int a=10;
+    int b=20;
+    printf("10 is not divisible by 20:",a%b);
+    return 0;
+
+}
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter value for 'a': ");
+    scanf("%d", &a);
+    printf("Enter value for 'b': ");
+    scanf("%d", &b);
+    c = a % b;
+    if (c == 0) {
+        printf("%d is divisible by %d\n", a, b);
+    } else {
+        printf("%d is not divisible by %d\n", a, b);
+    }
+    return 0;
+}
+*/
