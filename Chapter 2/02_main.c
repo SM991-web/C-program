694b0a8445f83f9cf9d17e7a39d87ca218487349
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-24 17:02:15 +0530

    commit

--- Chapter 2/02_main.c
+++ Chapter 2/02_main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#include <conio.h>
 int main(){
+    
     int a = 4;
     int b = 3;
     int c = 12;
     int d = a+b+c;
     printf("The value of a is %d b is %d c is %d and total is %d",a,b,c,d);
+    getch();
     return 0;
 
 }
