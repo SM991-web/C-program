2a343056cf670d61aae8eaf8125c74f30f03dc02
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-25 19:10:09 +0530

    commit

--- Chapter 3/03_main.c
+++ Chapter 3/03_main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int age = 18;
-    if (age>=18)
+    int age = 40;
+    if (age>=18 , age >=45)
     {
-        printf{"You are eligible to vote"};
+        printf("You are eligible to vote");
     }
     else if (age != 18)
     {
-        printf{"You are eligible to vote"};
+        printf("You are eligible to vote");
     }
     return 0;
 
