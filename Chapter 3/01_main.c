b6ac5f9da672aa8f744eaa19e585143ca4954c38
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-25 17:44:25 +0530

    commit

--- Chapter 3/01_main.c
+++ Chapter 3/01_main.c
@@ -7,14 +7,16 @@ int main()
     {
         printf("Your age is greater than 10");
     }
+    //if condition when executing conditional statements
     else if (age == 5)
     {
         printf("Your age is exactly equal to %d",age); 
     }
-
+    //else if statement is executed if  'if condition is not executed '
     else
     {
         printf("your age is less than 10");
     }
+    // else statement is executed if both the condition is not executed
     return 0;
 }
