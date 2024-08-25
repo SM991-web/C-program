bf1e556245001530c4c7a8c876bf489166a7ce11
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-25 23:11:43 +0530

    commit

--- Chapter 3/01_practice_set_Q1.c
+++ Chapter 3/01_practice_set_Q1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int a=10;
+    int a=12;
     if (a=11)
     {
         printf("I am 11");
     }
     else
     {
-        prints("I am not 11");
+        printf("I am not 11");
     }
     return 0;
 
