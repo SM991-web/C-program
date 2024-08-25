37906ebeee03dddb667d7b49c6cb9db4b7829db3
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-25 19:02:43 +0530

    commit

--- Chapter 3/02_mian.c
+++ Chapter 3/02_mian.c
@@ -6,6 +6,17 @@ int main(){
     printf("The value of %d or %d is %d\n",a,b,a||b);
     printf("The value of not(%d) is %d\n",a,!a);
     
+    if (a&&b)
+    {
+        printf("Both are true\n");
+    }
+    if (a)
+    {
+        if (b)
+        {
+            printf("Both are true\n");
+        }
+    }
     /* a&&b AND operators , a||b OR operators 
     !a NOT operators*/
     return 0;
---
+++ Chapter 3/03_main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int age = 18;
+    if (age>=18)
+    {
+        printf{"You are eligible to vote"};
+    }
+    else if (age != 18)
+    {
+        printf{"You are eligible to vote"};
+    }
+    return 0;
+
+}
