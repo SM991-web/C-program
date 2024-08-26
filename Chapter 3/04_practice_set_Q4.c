4427b2b10fda84a4b6dd21064d573d48d7b335c8
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-26 16:40:40 +0530

    commit

--- Chapter 3/04_practice_set_Q4.c
+++ Chapter 3/04_practice_set_Q4.c
@@ -3,7 +3,14 @@
 int main(){
     int Year;
     printf("Enter Year:");
-    scanf("%d",);
+    scanf("%d",&Year);
+    if (Year%4==0 && Year%10!=0 || Year%400==0)
+    {
+        printf("%d is leap year ",Year);
+    }
+    else{
+        printf("%d is not leap year ",Year);
+    }
     return 0;
 
 }
