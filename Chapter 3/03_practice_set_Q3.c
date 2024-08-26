6d57877573e45be7538862148498eeebf715518f
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-26 16:07:41 +0530

    commit

--- Chapter 3/03_practice_set_Q3.c
+++ Chapter 3/03_practice_set_Q3.c
@@ -9,22 +9,22 @@ int main(){
         printf("Your are not in the slab of paying Income Tax\n");
         printf("Thanks For your Support\n");
     }
-    else if (Income>250000.00 || Income<500000)
+    else if (Income>250000.00 && Income<500000)
     {
         printf("Your are in slot of Income tax\n");
-        printf("You need to pay Income Tax amount (INR):%.2f\n",(Income)*0.05);
+        printf("You need to pay Income Tax amount (INR):%.2f\n",(Income-250000)*0.05);
         printf("Thanks For your Support\n");
     }
-    else if (Income>500000.00 || Income<1000000)
+    else if (Income>500000.00 && Income<1000000)
     {
         printf("Your are in slot of Income tax\n");
-        printf("You need to pay Income Tax amount (INR):%.2f\n",(Income)*0.20);
+        printf("You need to pay Income Tax amount (INR):%.2f\n",(Income-500000)*0.20);
         printf("Thanks For your Support\n");
     }
     else if (Income>1000000)
     {
         printf("Your are in slot of Income tax\n");
-        printf("You need to pay Income Tax amount (INR):%.2f\n",(Income)*0.30);
+        printf("You need to pay Income Tax amount (INR):%.2f\n",(Income-1000000)*0.30);
         printf("Thanks For your Support\n");
     }
     return 0;
---
+++ Chapter 3/04_practice_set_Q4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int Year;
+    printf("Enter Year:");
+    scanf("%d",)
+    return 0;
+
+}
