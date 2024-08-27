0377e0a3ccf8399b9b639ec98ea5277d70f8bd57
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-27 23:07:36 +0530

    commit

---
+++ Chapter 4/01_main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    //print happy birthday 100000 times
+    printf("Happy Birthday");
+    return 0;
+    // while loop 
+    //do-while loop
+    //for loop
+}
---
+++ Chapter 4/02_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i = 1 ;
+    while (0<=4)
+    {
+        printf("Happy birthday\n");
+        i++;
+    }
+    
+    return 0;
+
+}
