50702708db98688f97b3b6d502f583b566639202
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-08-30 11:48:20 +0530

    commit

--- Chapter 4/04_main.c
+++ Chapter 4/04_main.c
@@ -6,10 +6,17 @@ int main(){
     i = i + 5;
     printf("The value of i is %d\n",i);
     /*
+    i += 2;
+    i -= 2; 
+    i *= 2;
+    i /= 2;
+    i %= 2; 
+    --i;
+    i--;
     printf("The value of i is %d\n",i++);
     i++ prints i first and then increments (Post Increment Operator)
     ++i increments first and then prints i (Pre Increment Operator)*/
-    printf("The value of i is %d\n",++i);
+    printf("The value of i is %d\n",i);
     return 0;
 
 }
