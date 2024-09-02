c9cfbeb347615c119c733c9d8e50d8aa8ee760c0
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-02 19:51:30 +0530

    commit

---
+++ Chapter 4/06_main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i =2;
+    scanf("enter value for n:%d\n",&n);
+    do
+    {
+        printf("%d\n",i);
+        i++;
+    } while (i<=n);
+    
+    return 0;
+}
+
