d67e5d5f4b8c954d4c2cf63ebfe369fd0993e56d
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-05 20:28:55 +0530

    commit

---
+++ Chapter 4/08_mai.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Value for n:\n");
+    scanf("%d",&n);
+    for (int i = 0; i <= n ; i++)
+    {
+        printf("Print natural numbers:%d\n",i);
+    }
+
+    return 0;
+
+}
+// 3:54:37
