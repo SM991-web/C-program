9ac246c7a45be4aa564528e547ced8ddfb9c65b7
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-05 19:29:56 +0530

    commit

--- Chapter 4/06_main.c
+++ Chapter 4/06_main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, i =2;
-    scanf("enter value for n:%d\n",&n);
+int main(){
+    int n,i=0;
+    printf("Enter value for n: ");
+    scanf("%d", &n);
     do
     {
-        printf("%d\n",i);
+        printf("Print i: %d\n", i);
         i++;
-    } while (i<=n);
+    } while (i <= n);
     
     return 0;
 }
-
---
+++ Chapter 4/07_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n = 6;
+    for (int i = 0; n > i; i++)
+    {
+        printf("The value of i is %d\n",i);
+    }
+    
+    return 0;
+
+}
