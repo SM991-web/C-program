4f5853a8082a29c91234bac9cd07dc01b1be15ec
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-06 19:29:57 +0530

    commit

--- Chapter 4/09_practice_set_Q9.c
+++ Chapter 4/09_practice_set_Q9.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int a=1,n;
-    for (int i = 1; i<=n; i++)
+    int a=1,n,i=1;
+    printf("Enter number:");
+    scanf("%d",&n);
+    for (; i<=n; i++)
     {
         a*=i;
+        printf("factorial  %d : %d",i,a);
     }
-    printf("factorial %d:",a);
+    printf("factorial of %d : %d",n,a);
     
     return 0;
 
