e5a944c0afda2d9e3466acf7e16198b2ffb66ae0
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-06 13:15:40 +0530

    commit

--- Chapter 4/02_practice_set_Q2.c
+++ Chapter 4/02_practice_set_Q2.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int n=10 ;
-    
-    // printf("Table of %d\n",n);
-    for(int i=10;i;i--)
+    int n ;
+    printf("Enter number:");
+    scanf("%d",&n);
+    printf("Table of %d\n",n);
+    for(int i=10;i>=0;i--)
     {
         printf("%d x %d = %d\n",i,n,(i*n));
     }
     return 0;
 
 }
+// #include <stdio.h>
+
+// int main(){
+//     int i = 1, n; 
+//     printf("Enter Number:");
+//     scanf("%d", &n);  
+
+//     printf("Table of %d\n", n);  
+//     do {
+//         printf("%d x %d = %d\n", i, n, (n * i));  
+//         i++;  
+//     } while (i <= 10);  
+    
+//     return 0;
+// }
