c98388dea468bd4b3fa356524174433bc350737a
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-07 19:42:51 +0530

    commit

--- Chapter 4/11_practice_set_Q11.c
+++ Chapter 4/11_practice_set_Q11.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-int main(){
-    int n,p=0;
-    printf("Enter Number:");
-    scanf("%d",&n);
-    for (int i = 2; i < n; i++)
-    {
-        if (n%i==0 && n!=0 )
-        {   
-            p=1;
-            break;
-        }
-        
-    }
-    if (p)
-    {           
-        printf("%d is not  prime",n);
-    }
-    else
-    {
-        printf("%d is  prime",n);
-    }
-    
-    
-    return 0;
+int main() {
+    int p = 0, n;
+    printf("Enter number: ");
+    scanf("%d", &n);
 
+    for (int i = 2; i <= n / 2; i++) {
+        if (n <= 1) {  
+            printf("%d is not prime", n);
+            return 0;  
+        }
+        if (n % i == 0) {
+            p = 1; 
+            printf("%d is not prime", n);    
+            break;     
+        }
+    }
+
+    if (p == 1) {
+        printf("%d is not prime", n);
+    } 
+    else {
+        printf("%d is prime", n);
+    }
+
+    return 0;
 }
