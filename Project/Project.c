d55d1195a2dc502056d59f2d51691d67604d74d3
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2024-09-07 15:55:08 +0530

    commit

---
+++ Project/Project.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator with current time
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = (rand() % 100) + 1;
+    int no_of_guess=0;
+    int guessed;
+
+    // Print the random number
+    // printf("Random number between 1 and 100: %d\n", randomNumber);
+    do
+    {
+        printf("Guess tha number:");
+        scanf("%d",&guessed);
+        if (guessed>randomNumber)       
+        {
+            printf("Hint: Lower number\n");
+        }
+        else if (guessed<randomNumber)
+        {
+            printf("Hint: Higher number\n");
+        }
+        no_of_guess++;
+        
+    } while (guessed!=randomNumber);
+    
+    printf("You guessed the number correctly in %d guesses ",no_of_guess);
+    return 0;
+}
